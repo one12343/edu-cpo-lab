52855a58dd371b413dcba20ece11449dafa79671
one12343 <53133053+one12343@users.noreply.github.com>
2022-05-29 00:44:35 +0800

    Add files via upload

--- dynamic_array.py
+++ dynamic_array.py
@@ -15,8 +15,6 @@ class Dynamic_array:
 
     def __eq__(self, other):
         return (self.__class__ == other.__class__ and
-                # self._n == other._n and
-                # self._capacity == other._capacity and
                 operator.eq(get_array(self), get_array(other))
                 )
 
@@ -26,193 +24,189 @@ class Dynamic_array:
     def __setitem__(self, index, newItem):
         self._A[index] = newItem
 
-
-def get_len(Dynamic_array):
-    return Dynamic_array._n
-
-
-def is_empty(Dynamic_array):
-    return Dynamic_array._n == 0
+    def __copy__(self):
+        return self
 
 
-def get_item(Dynamic_array, k):
-    if not 0 <= k < Dynamic_array._n:
+def get_len(dynamic_array):
+    return dynamic_array._n
+
+
+def is_empty(dynamic_array):
+    return dynamic_array._n == 0
+
+
+def get_item(dynamic_array, k):
+    if not 0 <= k < dynamic_array._n:
         raise ValueError('invalid index')
-    return Dynamic_array._A[k]
+    return dynamic_array._A[k]
 
 
-def make_array(Dynamic_array, c):
+def make_array(dynamic_array, c):
     return (c * ctypes.py_object)()
 
 
-def array_resize(Dynamic_array, c):
-    B = make_array(Dynamic_array, c)
-    # After expansion, you need to copy the elements
-    # in the original array to the new array
-    for k in range(Dynamic_array._n):
-        B[k] = Dynamic_array._A[k]
-    Dynamic_array._A = B
-    Dynamic_array._capacity = c
-    return Dynamic_array
+def array_resize(dynamic_array, c):
+    B = make_array(dynamic_array, c)
+    for k in range(dynamic_array._n):
+        B[k] = dynamic_array._A[k]
+    dynamic_array._A = B
+    dynamic_array._capacity = c
+    return dynamic_array
 
 
-def append(Dynamic_array0, obj):
-    Dynamic_array = new_item(Dynamic_array0)
-    # When the current element of the array is full,
-    # the expansion capacity is twice that of the original one, and it is added
-    if Dynamic_array._n == Dynamic_array._capacity:
-        Dynamic_array = array_resize(Dynamic_array,
-                                     2 * Dynamic_array._capacity)
-    Dynamic_array._A[Dynamic_array._n] = obj
-    Dynamic_array._n += 1
-    return Dynamic_array
+def append(dynamic_array, obj):
+    if dynamic_array._n == dynamic_array._capacity:
+        dynamic_array = array_resize(dynamic_array,
+                                     2 * dynamic_array._capacity)
+    dynamic_array._A[dynamic_array._n] = obj
+    dynamic_array._n += 1
+    return dynamic_array
 
 
-def remove_value(Dynamic_array0, value):
-    Dynamic_array = new_item(Dynamic_array0)
-    for k in range(Dynamic_array._n):
-        if Dynamic_array._A[k] == value:
-            for j in range(k, Dynamic_array._n - 1):
-                Dynamic_array._A[j] = Dynamic_array._A[j + 1]
-            Dynamic_array._A[Dynamic_array._n - 1] = None
-            Dynamic_array._n -= 1
-            return Dynamic_array
+def remove_value(dynamic_array1, value):
+    dynamic_array = new_item(dynamic_array1)
+    for k in range(dynamic_array._n):
+        if dynamic_array._A[k] == value:
+            for j in range(k, dynamic_array._n - 1):
+                dynamic_array._A[j] = dynamic_array._A[j + 1]
+            dynamic_array._A[dynamic_array._n - 1] = None
+            dynamic_array._n -= 1
+            return dynamic_array
     raise ValueError('value not found')
 
 
-def remove_index(Dynamic_array0, index):
-    Dynamic_array = new_item(Dynamic_array0)
-    if index >= Dynamic_array0._n:
+def remove_index(dynamic_array1, index):
+    dynamic_array = new_item(dynamic_array1)
+    if index >= dynamic_array._n:
         ValueError('index out of line')
 
-    for k in range(Dynamic_array._n):
+    for k in range(dynamic_array._n):
         if k == index:
-            for j in range(k, Dynamic_array._n - 1):
-                Dynamic_array._A[j] = Dynamic_array._A[j + 1]
-            Dynamic_array._A[Dynamic_array._n - 1] = None
-            Dynamic_array._n -= 1
-            return Dynamic_array
+            for j in range(k, dynamic_array._n - 1):
+                dynamic_array._A[j] = dynamic_array._A[j + 1]
+            dynamic_array._A[dynamic_array._n - 1] = None
+            dynamic_array._n -= 1
+            return dynamic_array
 
 
-def member(value, Dynamic_array):
-    if Dynamic_array is None:
+def member(value, dynamic_array):
+    if dynamic_array is None:
         arr_len = 0
     else:
-        arr_len = Dynamic_array._n
+        arr_len = dynamic_array._n
     for k in range(arr_len):
-        if Dynamic_array._A[k] == value:
+        if dynamic_array._A[k] == value:
             return True
     return False
 
 
-def print_all(Dynamic_array):
-    for i in range(Dynamic_array._n):
-        print(Dynamic_array._A[i])
+def print_all(dynamic_array):
+    for i in range(dynamic_array._n):
+        print(dynamic_array._A[i])
 
 
-def get_array(Dynamic_array):
+def get_array(dynamic_array):
     temp_list = []
-    for i in range(Dynamic_array._n):
-        temp_list.append(Dynamic_array._A[i])
+    for i in range(dynamic_array._n):
+        temp_list.append(dynamic_array._A[i])
     return temp_list
 
 
-def cons(a, Dynamic_array1):
-    Dynamic_array0 = new_item(Dynamic_array1)
-
+def cons(a, dynamic_array0):
+    dynamic_array = new_item(dynamic_array0)
     if type(a) != list:
-        if Dynamic_array0._n == Dynamic_array0._capacity:
-            Dynamic_array0 = array_resize(Dynamic_array0,
-                                          2 * Dynamic_array0._capacity)
+        if dynamic_array._n == dynamic_array._capacity:
+            dynamic_array = array_resize(dynamic_array,
+                                          2 * dynamic_array._capacity)
 
-        for i in range(Dynamic_array0._n-1, -1, -1):
-            Dynamic_array0._A[i+1] = Dynamic_array0._A[i]
+        for i in range(dynamic_array._n-1, -1, -1):
+            dynamic_array._A[i+1] = dynamic_array._A[i]
 
-        Dynamic_array0._n += 1
-        Dynamic_array0._A[0] = a
+        dynamic_array._n += 1
+        dynamic_array._A[0] = a
     elif type(a) == list:
-        if Dynamic_array0._n+len(a) > Dynamic_array0._capacity:
-            Dynamic_array0 = array_resize(Dynamic_array0,
-                                          2 * Dynamic_array0._capacity)
+        if dynamic_array._n+len(a) > dynamic_array._capacity:
+            dynamic_array = array_resize(dynamic_array,
+                                          2 * dynamic_array._capacity)
 
         new_empty = Dynamic_array()
         for i in a:
             new_empty = append(new_empty, i)
-        for i in range(Dynamic_array0._n):
-            new_empty = append(new_empty, Dynamic_array0._A[i])
-        Dynamic_array0 = new_empty
-    return Dynamic_array0
+        for i in range(dynamic_array._n):
+            new_empty = append(new_empty, dynamic_array._A[i])
+        dynamic_array = new_empty
+    return dynamic_array
 
 
-def new_item(Dynamic_array0):
-    new_Dynamic_array = Dynamic_array(Dynamic_array0._capacity)
-    new_Dynamic_array._n = Dynamic_array0._n
-    new_Dynamic_array._capacity = Dynamic_array0._capacity
-    for i in range(Dynamic_array0._n):
-        new_Dynamic_array._A[i] = Dynamic_array0._A[i]
-    return new_Dynamic_array
+def new_item(dynamic_array):
+    new_dynamic_array = Dynamic_array(dynamic_array._capacity)
+    new_dynamic_array._n = dynamic_array._n
+    new_dynamic_array._capacity = dynamic_array._capacity
+    for i in range(dynamic_array._n):
+        new_dynamic_array._A[i] = dynamic_array._A[i]
+    return new_dynamic_array
 
 
-def reverse_arr(Dynamic_array1):
-    Dynamic_array0 = new_item(Dynamic_array1)
+def reverse_arr(dynamic_array):
     temp_list = []
-    for i in range(Dynamic_array0._n):
-        temp_list.append(Dynamic_array0._A[i])
+    for i in range(dynamic_array._n):
+        temp_list.append(dynamic_array._A[i])
     temp_list.reverse()
-    for i in range(Dynamic_array0._n):
-        Dynamic_array0._A[i] = temp_list[i]
-    return Dynamic_array0
+    for i in range(dynamic_array._n):
+        dynamic_array._A[i] = temp_list[i]
+    return dynamic_array
 
 
-def to_list(Dynamic_array):
-    return get_array(Dynamic_array)
+def to_list(dynamic_array):
+    return get_array(dynamic_array)
 
 
 def from_list(temp_list):
-    Dynamic_array0 = Dynamic_array()
-    while(Dynamic_array0._capacity < len(temp_list)):
-        Dynamic_array0 = array_resize(Dynamic_array0, 2 *
-                                      Dynamic_array0._capacity)
-    Dynamic_array0._n = len(temp_list)
-    for i in range(Dynamic_array0._n):
-        Dynamic_array0._A[i] = temp_list[i]
-    return Dynamic_array0
+    dynamic_array = Dynamic_array()
+    while(dynamic_array._capacity < len(temp_list)):
+        dynamic_array = array_resize(dynamic_array, 2 *
+                                      dynamic_array._capacity)
+    dynamic_array._n = len(temp_list)
+    for i in range(dynamic_array._n):
+        dynamic_array._A[i] = temp_list[i]
+    return dynamic_array
 
 
-def concat(Dynamic_array1, Dynamic_array2):
-    temp_list = get_array(Dynamic_array1)
-    return cons(temp_list, Dynamic_array2)
+def concat(dynamic_array1, dynamic_array2):
+    temp_list = get_array(dynamic_array1)
+    return cons(temp_list, dynamic_array2)
 
 
-def filter(Dynamic_array1, new_function):
-    Dynamic_array0 = new_item(Dynamic_array1)
+def filter(dynamic_array1, new_function):
+    dynamic_array = new_item(dynamic_array1)
     temp_list = []
-    for i in range(Dynamic_array0._n):
-        flag = new_function(Dynamic_array0._A[i])
+    for i in range(dynamic_array._n):
+        flag = new_function(dynamic_array._A[i])
 
         if flag is False:
-            temp_list.append(Dynamic_array0._A[i])
+            temp_list.append(dynamic_array._A[i])
     for i in temp_list:
-        Dynamic_array0 = remove_value(Dynamic_array0, i)
-    return Dynamic_array0
+        dynamic_array = remove_value(dynamic_array, i)
+    return dynamic_array
 
 
-def map(Dynamic_array1, new_function):
-    Dynamic_array0 = new_item(Dynamic_array1)
-    for i in range(Dynamic_array0._n):
-        Dynamic_array0._A[i] = new_function(Dynamic_array0._A[i])
-    return Dynamic_array0
+def map(dynamic_array1, new_function):
+    dynamic_array = new_item(dynamic_array1)
+    for i in range(dynamic_array._n):
+        dynamic_array._A[i] = new_function(dynamic_array._A[i])
+    return dynamic_array
 
 
-def reduce(Dynamic_array1, new_function):
-    Dynamic_array0 = new_item(Dynamic_array1)
-    temp = Dynamic_array0._A[0]
+def reduce(dynamic_array1, new_function):
+    dynamic_array = new_item(dynamic_array1)
+    temp = dynamic_array._A[0]
     result = 0
-    for i in range(1, Dynamic_array0._n):
-        result = new_function(temp, Dynamic_array0._A[i])
+    for i in range(1, dynamic_array._n):
+        result = new_function(temp, dynamic_array._A[i])
         temp = result
     return result
 
 
-def empty(Dynamic_array):
-    return Dynamic_array._n == 0
+def empty(dynamic_array):
+    return dynamic_array._n == 0
--- dynamic_array_test.py
+++ dynamic_array_test.py
@@ -1,6 +1,6 @@
 import unittest
-# from hypothesis import given, settings
-# import hypothesis.strategies as st
+from hypothesis import given, settings
+import hypothesis.strategies as st
 from dynamic_array import get_len, get_item, reverse_arr, to_list, from_list
 from dynamic_array import append, remove_value, remove_index, member, concat
 from dynamic_array import get_array, cons, filter, map, reduce, empty
@@ -9,11 +9,9 @@ import math
 
 
 class TestMutableULList(unittest.TestCase):
-    # first we use unit test!
 
-    def test_api(self):
 
-        # print('\nTesting cons...')
+    def test_cons(self):
         array = Dynamic_array()
         l1 = cons(None, cons(1, array))
         l2 = cons(1, cons(None, array))
@@ -27,57 +25,50 @@ class TestMutableULList(unittest.TestCase):
         self.assertEqual(str(l1), str(cons(None, cons(1, array))))
         self.assertEqual(str(l3), "[3, 2, None]")
 
-        # print('\nTesting length...')
-        self.assertEqual(get_len(array), 0)
-        self.assertEqual(get_len(l1), 2)
-        self.assertEqual(get_len(l2), 2)
+    def length(self):
+        x = [1, 2, 3]
+        x2 = [1, 2, 3, 4, 5, 6]
+        array = from_list(x)
+        array2 = from_list(x2)
+        self.assertEqual(get_len(array), 3)
+        self.assertEqual(get_len(array2), 6)
 
-        # print('\nTesting remove...')
-        self.assertEqual(get_array(remove_index(l1, 0)), [1])
-        self.assertEqual(get_array(remove_index(l1, 1)), [None])
-        array2 = Dynamic_array()
-        temp_list = [1, 2, 3, 4, 5, 6]
-        for i in temp_list:
-            array2 = append(array2, i)
+    def test_remove(self):
+        x = [1, 2, 3]
+        array = from_list(x)
+        self.assertEqual(get_array(remove_index(array, 0)), [2, 3])
+        self.assertEqual(get_array(remove_index(array, 1)), [1, 3])
+
+        x2 = [1, 2, 3, 4, 5, 6]
+        array2 = from_list(x2)
         self.assertEqual(get_array(remove_value(array2, 3)), [1, 2, 4, 5, 6])
         self.assertEqual(get_array(remove_value(array2, 5)), [1, 2, 3, 4, 6])
 
-        # print('\nTesting member...')
+    def test_member(self):
+        x = [1, 2, 3, 4, 5, 6]
+        array = from_list(x)
+        x2 = [1, 2, None, 4, 5, 6]
+        array2 = from_list(x2)
         self.assertFalse(member(None, array))
-        self.assertTrue(member(None, l1))
-        self.assertTrue(member(1, l1))
-        self.assertFalse(member(2, l1))
+        self.assertTrue(member(None, array2))
+        self.assertTrue(member(1, array))
+        self.assertFalse(member(3, array2))
 
-        # print('\nTesting reverse...')
-        self.assertEqual(l1, reverse_arr(l2))
-        temp_list_reverse = [6, 5, 4, 3, 2, 1]
-        array5 = Dynamic_array()
-        array6 = Dynamic_array()
-        for i in temp_list:
-            array5 = append(array5, i)
-        for i in temp_list_reverse:
-            array6 = append(array6, i)
-        self.assertEqual(array6, reverse_arr(array5))
+    def test_reverse(self):
+        x1 = [1, 2, 3, 4, 5, 6]
+        x2 = [6, 5, 4, 3, 2, 1]
+        array1 = from_list(x1)
+        array2 = from_list(x2)
+        self.assertEqual(array1, reverse_arr(array2))
 
-        # print('\nTesting to_list and from_list...')
-        self.assertEqual(to_list(l1), [None, 1])
-        self.assertEqual(l1, from_list([None, 1]))
+    def test_concat(self):
+        l1 = [None, 1]
+        l2 = [1, None]
+        array1 = from_list(l1)
+        array2 = from_list(l2)
+        self.assertEqual(concat(array1, array2), from_list([None, 1, 1, None]))
 
-        # print('\nTesting concat...')
-        self.assertEqual(concat(l1, l2), from_list([None, 1, 1, None]))
-
-        # print('\nTesting append and getitem...')
-        buf = []
-        for e in l1:
-            buf.append(e)
-        self.assertEqual(buf, [None, 1])
-        lst = to_list(l1) + to_list(l2)
-        for e in l1:
-            lst.remove(e)
-        for e in l2:
-            lst.remove(e)
-        self.assertEqual(lst, [])
-
+    def test_append_and_getitem(self):
         array = Dynamic_array()
         temp_list = [1, 2, 3, 4, 5, 6]
         for i in temp_list:
@@ -89,67 +80,58 @@ class TestMutableULList(unittest.TestCase):
         temp1 = get_item(array, 5)
         self.assertEqual(temp1, temp_list[5])
 
-        # print('\nRewrite_eq and test eq...')
-        array3 = Dynamic_array()
-        array4 = Dynamic_array()
-        self.assertEqual(array3, array4)
-        for i in temp_list:
-            array3 = append(array3, i)
-            array4 = append(array4, i)
-        self.assertEqual(array3, array4)
-
-        # print('\nTest_change_value...')
-        array3[3] = 0
-        self.assertEqual(str(array3), "[1, 2, 3, 0, 5, 6]")
-
-        # print('\nTest_filter...')
-        array = Dynamic_array()
-        temp_list = [1, 4, 6, 7, 9, 12, 17]
-        for i in temp_list:
-            array = append(array, i)
-        temp1 = filter(array, is_odd)  # (array,function)
+    def test_filter(self):
+        x = [1, 4, 6, 7, 9, 12, 17]
+        array = from_list(x)
+        temp1 = filter(array, is_odd)
         self.assertEqual(str(temp1), "[1, 7, 9, 17]")
 
-        array = Dynamic_array()
-        temp_list = ['test', None, '', 'str', ' ', 'END']
-        for i in temp_list:
-            array = append(array, i)
-        temp2 = filter(array, is_not_empty)  # (array,function)
+        x2 = ['test', None, '', 'str', ' ', 'END']
+        array2 = from_list(x2)
+        temp2 = filter(array2, is_not_empty)
         self.assertEqual(str(temp2), "['test', 'str', 'END']")
 
-        array = Dynamic_array()
-        temp_list = [1, 2, 3, 2, 5, 2, 7, 2, 9]
-        for i in temp_list:
-            array = append(array, i)
-        temp3 = filter(array, remove_value_2)  # (array,function)
+        x3 = [1, 2, 3, 2, 5, 2, 7, 2, 9]
+        array3 = from_list(x3)
+        temp3 = filter(array3, remove_value_2)
         self.assertEqual(str(temp3), "[1, 3, 5, 7, 9]")
 
-        array = Dynamic_array()
+        array4 = from_list([])
         for i in range(1, 101):
-            array = append(array, i)
-        temp4 = filter(array, is_sqr)  # (array,function)
+            array4 = append(array4, i)
+        temp4 = filter(array4, is_sqr)
         self.assertEqual(str(temp4), "[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]")
 
-        # print('\nTest_map...')
-        array = Dynamic_array()
-        temp_list = [2, 3, 4, 5, 6, 7, 8, 9]
-        for i in temp_list:
-            array = append(array, i)
+        x4 = [1, 2, 3, 4, 5]
+        array5 = from_list(x4)
+        self.assertEqual([2, 4], to_list(filter(array5, lambda e: e % 2 == 0)))
+
+    def test_map(self):
+        array = from_list([2, 3, 4, 5, 6, 7, 8, 9])
         temp = map(array, my_square)
         self.assertEqual(str(temp), "[4, 9, 16, 25, 36, 49, 64, 81]")
 
         temp2 = map(temp, my_sqr)
-        self.assertEqual(str(temp2), str(temp_list))  # [2,3,4,5,6,7,8,9]
+        self.assertEqual(str(temp2), str(array))
 
-        # print('\nTest_reduce...')
         array = Dynamic_array()
-        temp_list = [1, 2, 3, 4, 5]
-        for i in temp_list:
-            array = append(array, i)
+        self.assertEqual(to_list(map(array, str)), [])
+        array = from_list([1, 2, 3])
+        self.assertEqual(to_list(map(array, str)), ["1", "2", "3"])
+        self.assertEqual(to_list(map(array, lambda x: x + 1)), [2, 3, 4])
+
+    def test_reduce(self):
+        array = from_list([1, 2, 3, 4, 5])
         result = reduce(array, my_add)
         self.assertEqual(str(result), "15")
+        self.assertEqual(reduce(array, lambda st, e: st + e), 15)
 
-        # print('\nTest_empty...')
+        array = from_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        result = reduce(array, my_add)
+        self.assertEqual(str(result), "55")
+        self.assertEqual(reduce(array, lambda st, e: st + e), 55)
+
+    def test_empty(self):
         array = Dynamic_array()
         self.assertEqual(empty(array), True)
         array = append(array, 1)
@@ -159,8 +141,46 @@ class TestMutableULList(unittest.TestCase):
         array = append(array, 2)
         self.assertFalse(empty(array))
 
+    def test_iter(self):
+        x = [1, 2, 3]
+        array = from_list(x)
+        tmp = []
+        try:
+            it = iter(array)
+            while True:
+                tmp.append(next(it))
+        except StopIteration:
+            pass
+        self.assertEqual(x, tmp)
+        self.assertEqual(to_list(array), tmp)
+        it = iter(Dynamic_array())
+        self.assertRaises(StopIteration, lambda: next(it))
 
-# print('\nThese are functions to test filter')
+    @given(st.lists(st.integers()))
+    def test_from_list_to_list_equality(self, a):
+        self.assertEqual(to_list(from_list(a)), a)
+
+    @given(st.lists(st.integers()))
+    def test_monoid_identity(self, lst):
+        a = from_list(lst)
+        empty = Dynamic_array()
+        self.assertEqual(concat(empty, a), a)
+        self.assertEqual(concat(a, empty), a)
+
+    @given(x1 = st.lists(st.integers()),
+           x2 = st.lists(st.integers()),
+           x3 = st.lists(st.integers()))
+    def test_monoid_associativity(self, x1, x2, x3):
+        a = from_list(x1)
+        b = from_list(x2)
+        c = from_list(x3)
+
+        # mconcar: (a*b)*c = a*(b*c)
+        self.assertEqual(concat(a, concat(b, c)),
+                         concat(concat(a, b), c))
+
+
+# functions to test filter
 def is_odd(x):
     return x % 2 == 1
 
@@ -181,7 +201,7 @@ def remove_value_2(x):
     return x != 2
 
 
-# print('\nThese are functions to test map')
+# These are functions to test map
 def my_square(n):
     return n * n
 
@@ -190,7 +210,7 @@ def my_sqr(x):
     return int(math.sqrt(x))
 
 
-# print('\nTest of function of reduce')
+# Test of function of reduce
 def my_add(x, y):
     return x+y
 
